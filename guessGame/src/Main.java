9a0dbf78332b12d3fe07db810253c37f0112d157
REDACTED <90115290+GlitchGamer1459@users.noreply.github.com>
2021-09-26 17:39:04 -0700

    Add files via upload

---
+++ guessGame/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    //when called, runs a new scan request through the terminal
+    public static void Scan(int random, Scanner scanner) {
+        System.out.print("Type a guess in: ");
+        try {
+            int input = scanner.nextInt();
+            if (input < random && input <= 1000 && input >= 0) {
+                System.out.println("Bigger");
+                Scan(random, scanner);
+            } else if (input > random && input <= 1000 && input >= 0) {
+                System.out.println("Smaller");
+                Scan(random, scanner);
+            } else if (input == random && input <= 1000 && input >= 0) {
+                System.out.print("Correct!\nYou Win!");
+            } else if (input > 1000 || input < 0) {
+                if (input == 5109) {
+                    System.out.print("Stop key received, stopping...\nStopped.");
+                } else {
+                    System.out.print("Input out of range.");
+                    Scan(random, scanner);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Please input a whole number from 0-1000.");
+            scanner.nextLine();
+            Scan(random, scanner);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = (int)(Math.random() * 1000);
+
+        System.out.println("A random number from 0-1000 has been generated.\nEnter a guess within the range and the program will say if it's bigger or smaller.");
+        Scan(number, scanner);
+    }
+}
