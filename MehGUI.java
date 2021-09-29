449a50cfd047fc77e20eaecc593ad148997c3a1b
GlitchGamer1459 <glitchgamer1459@gmail.com>
2021-09-28 22:16:39 -0700

    Created an empty GUI window

---
+++ MehGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MehGUI {
+
+    public static void GUI(){
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout());
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        GUI();
+    }
+}
